a9ee29bc7dee83f7d2b16a4a872cdf3cbd0bc34b
Ernest <ernest.bruno94@yahoo.com>
2018-04-20 11:48:45 -0700

    Update Error404.jsx'

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,1 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default function Error404(){
+  return(
+    <div>
+      <h1>The page you are looking for does not exist!</h1>
+      <h2>Click <Link to="/">here</Link> to return home</h2>
+    </div>
+  );
+}
