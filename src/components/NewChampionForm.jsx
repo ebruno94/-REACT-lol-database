9aab124fe8724bfada86da3e4a3e6877f32dda5b
Ernest <ernest.bruno94@yahoo.com>
2018-04-20 11:08:04 -0700

    Create content for new ticket form. Add submission handler but information doesn't go anywhere

---
+++ src/components/NewChampionForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+function NewTicketForm(){
+  let _image = null;
+  let _name = null;
+  let _role = null;
+
+  function handleSubmission(event){
+    event.preventDefault();
+    _image.value = '';
+    _name.value = '';
+    _role.value = '';
+  }
+
+  return(
+    <div>
+      <h1>Fill out the form below with the necessary information</h1>
+      <hr/>
+      <form onSubmit={handleSubmission}>
+        <input
+          type='text'
+          id='image'
+          placeholder='Image URL path'
+          ref={(input) => {_image = input;}}/>
+        <input
+          type='text'
+          id='name'
+          placeholder='Champion Name'
+          ref={(input) => {_name = input;}}/>
+        <input
+          type='text'
+          id='name'
+          placeholder='Role'
+          ref={(input) => {_role = input;}}/>
+        <button type='submit'>Add</button>
+      </form>
+    </div>
+  );
+}
