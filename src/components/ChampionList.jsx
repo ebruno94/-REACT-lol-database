15ffee7b910d91ee576f1658a9b7ac1fadf6a45d
Ernest <ernest.bruno94@yahoo.com>
2018-04-20 11:00:34 -0700

    Finish contents of championList.jsx, no styles yet

--- src/components/ChampionList.jsx
+++ src/components/ChampionList.jsx
@@ -1,2 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+export default function ChampionList(props){
+  return(
+    <div>
+      <style jsx>{`
+
+
+
+
+
+
+
+      `}</style>
+      <div className='container'>
+        <div className='box'>
+          {props.championList.map((champ, index)=>
+            <Champion
+              image = {champ.image}
+              name = {champ.name}
+              role = {champ.role} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ChampionList.propTypes = {
+  championList = PropTypes.array
+}
